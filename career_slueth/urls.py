a31a824766f1b742776e73f1fc83da3c3543ce8a
mew101 <mshwss@gmail.com>
2020-10-13 22:35:37 -0400

    Initial setup

---
+++ career_slueth/urls.py
@@ -0,0 +1,44 @@
+"""career_slueth URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import *
+
+
+class Index(TemplateView):
+    template_name= "index.html"
+
+class Dashboard(TemplateView):
+    template_name= "dashboard.html"
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path ('', Index.as_view(),name='index'),
+    path ('dashboard/', Dashboard.as_view()),
+
+    path('signup/', SignUpView.as_view(), name="signup"),
+    #path('login/', Login.as_view(), name="login"),
+    #path('logout/', Logout.as_view(), name="logout"),
+]
